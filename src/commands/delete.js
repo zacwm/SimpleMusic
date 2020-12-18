41e805f768c8288a2142db61bc8a705f59e871c5
Enrico <franciscono.enry@gmail.com>
2020-12-18 21:23:22 +0100

    added last option to delete command

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -2,7 +2,10 @@
 const sm = require("../index");
 
 sm.command(["delete", "d"], async (msg) => {
-    if (!isInteger(msg.args[1]) || msg.args[1] <= 0 || msg.args[1] >= sm.data[msg.guild.id].queue.lenght) {
+    if (msg.args[1] == "last") {
+        sm.data[msg.guild.id].queue.splice(sm.data[msg.guild.id].queue.lenght - 1, 1);
+        msg.react("🗑");
+    } else if (!isInteger(msg.args[1]) || msg.args[1] <= 0 || msg.args[1] >= sm.data[msg.guild.id].queue.lenght) {
         msg.react("❌");
     } else {
         sm.data[msg.guild.id].queue.splice((msg.args[1] || 1) - 1, 1);
