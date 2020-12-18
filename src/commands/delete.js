833a23928a088e769b01e224f284b2acb62c1743
Enrico <franciscono.enry@gmail.com>
2020-12-18 21:17:31 +0100

    Delete command
    Yep, even more cool shit

---
+++ src/commands/delete.js
@@ -0,0 +1,11 @@
+// SimpleMusic - Command
+const sm = require("../index");
+
+sm.command(["delete", "d"], async (msg) => {
+    if (!isInteger(msg.args[1]) || msg.args[1] <= 0 || msg.args[1] >= sm.data[msg.guild.id].queue.lenght) {
+        msg.react("❌");
+    } else {
+        sm.data[msg.guild.id].queue.splice((msg.args[1] || 1) - 1, 1);
+        msg.react("🗑");
+    }
+});
