9ae5ee32ebf0f5ee03d3d153b9d6c555a042bda8
Enrico <franciscono.enry@gmail.com>
2020-12-19 16:47:31 +0100

    Fixing . . .

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -3,10 +3,13 @@ const sm = require("../index");
 
 sm.command(["delete", "d"], async (msg) => {
     if (msg.args[1] == "last") {
-        sm.data[msg.guild.id].queue.splice(sm.data[msg.guild.id].queue.lenght - 1, 1);
+        sm.data[msg.guild.id].queue.splice(sm.data[msg.guild.id].queue.length - 1, 1);
         msg.react("🗑");
-    } else if (!isInteger(msg.args[1]) || msg.args[1] <= 0 || msg.args[1] >= sm.data[msg.guild.id].queue.lenght) {
-        msg.react("❌");
+    } else if (!isInteger(msg.args[1]) || msg.args[1] <= 0 || msg.args[1] >= sm.data[msg.guild.id].queue.length) {
+        msg.channel.send("", {embed: {
+            color: msg.colors.warn,
+            title: "Invalid input"
+        }});
     } else {
         sm.data[msg.guild.id].queue.splice((msg.args[1] || 1) - 1, 1);
         msg.react("🗑");
--- src/commands/stop.js
+++ src/commands/stop.js
@@ -3,7 +3,6 @@ const sm = require("../index");
 
 sm.command(["stop", "disconnect", "leave"], (msg) => {
     let cmdGuild = sm.data[msg.guild.id];
-    sm.data[msg.guild.id].loop = false;
     if (cmdGuild && cmdGuild.voiceConnection) cmdGuild.voiceConnection.disconnect();
     msg.react("👍");
 });
