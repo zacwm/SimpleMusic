2cb84b85226b375f2c4699480e71373a0b7b8244
Zachary <me@zachary.fun>
2020-12-20 16:44:05 +1000

    Cleanup and colour fix.
    The warn and error shouldn't be used for successful setting changes.

--- src/commands/loop.js
+++ src/commands/loop.js
@@ -2,17 +2,9 @@
 const sm = require("../index");
 
 sm.command(["loop", "l"], async (msg) => {
-    if (sm.data[msg.guild.id].loop) {
-        sm.data[msg.guild.id].loop = false;
-        sm.data[msg.guild.id].statusMessage = await msg.channel.send("", {embed: {
-            color: msg.colors.warn,
-            title: "Looping disabled"
-        }});
-    } else {
-        sm.data[msg.guild.id].loop = true;
-        sm.data[msg.guild.id].statusMessage = await msg.channel.send("", {embed: {
-            color: msg.colors.ok,
-            title: "Looping enabled"
-        }});
-    }
+    sm.data[msg.guild.id].loop = !sm.data[msg.guild.id].loop;
+    sm.data[msg.guild.id].statusMessage = await msg.channel.send("", {embed: {
+        color: msg.colors.ok,
+        description: `Looping is now **${(sm.data[msg.guild.id].loop) ? "enabled" : "disabled"}**`
+    }});
 });
