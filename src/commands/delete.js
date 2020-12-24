ef94fd4107ee26f6c33b1fba430ae3f598ba131e
Enrico <franciscono.enry@gmail.com>
2020-12-24 09:33:08 +0100

    Queue bug fix

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -6,13 +6,13 @@ sm.command(["delete", "d"], async (msg) => {
     if (msg.args[1] == "last") {
         player.players[msg.guild.id].queue.splice(player.players[msg.guild.id].queue.length - 1, 1);
         msg.react("🗑");
-    } else if (!Number.isInteger(msg.args[1]) || msg.args[1] <= 0 || msg.args[1] >= player.players[msg.guild.id].queue.length) {
+    } else if (!Number.isInteger(parseInt(msg.args[1])) || parseInt(msg.args[1]) <= 0 || parseInt(msg.args[1]) > player.players[msg.guild.id].queue.length) {
         msg.channel.send("", {embed: {
             color: msg.colors.warn,
             title: "Invalid input"
         }});
     } else {
-        player.players[msg.guild.id].queue.splice((msg.args[1] || 1) - 1, 1);
+        player.players[msg.guild.id].queue.splice((parseInt(msg.args[1]) || 1) - 1, 1);
         msg.react("🗑");
     }
 });
