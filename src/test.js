62f23f34867f7d1ef4f614006a7d70547769ad9d
Zachary <me@zachary.fun>
2020-12-29 00:45:08 +1000

    Delete test file

--- src/test.js
+++
@@ -1,9 +0,0 @@
-function woop() {
-    return new Promise((res)=> {console.log("before");for(i = 0; i<2;i++){return res("middle"+i)};console.log("after");})
-}
-
-async function woopwoop() {
-    console.log(await woop());
-}
-
-woopwoop();
