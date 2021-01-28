347ff74df324a1828b6ff7d25c5e4b525ed31095
Enrico <franciscono.enry@gmail.com>
2021-01-28 11:00:10 +0100

    added bandcamp support
    added bandcamp support (direct link only for now as i don't know of an api that i could use)
    
    also fixed the previous fix (lol)

--- src/commands/player.js
+++ src/commands/player.js
@@ -116,7 +116,7 @@ exports.getQuery = (query, opts) => {
 
         for (j = 0; j < sourcesNames.length; j++) {
             if (sources[sourcesNames[j]].url.test(query)) {
-                let results = await sources[sourcesNames[j]].getInfo(sources[sourcesNames[j]].url.match(query)[0]).catch(e => {
+                let results = await sources[sourcesNames[j]].getInfo(query.match(sources[sourcesNames[j]].url)[0]).catch(e => {
                     reject(e);
                 });
                 results.forEach(songData => {
