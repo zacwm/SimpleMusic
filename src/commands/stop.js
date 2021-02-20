4d3324f2c216e1ab97f6da52038c13ac772718ff
Enrico <franciscono.enry@gmail.com>
2021-02-20 20:31:37 +0100

    Updated ytdl-core

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -5,5 +5,6 @@ const player = require("./player");
 sm.command(["stop", "disconnect", "leave"], (msg) => {
     let cmdGuild = player.players[msg.guild.id];
     if (cmdGuild && cmdGuild.voiceConnection) cmdGuild.voiceConnection.disconnect();
+    msg.channel.stopTyping(true);
     msg.react("👍");
 });
