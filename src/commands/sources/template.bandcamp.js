cb1de0ebb564f68dabb3bb885476275987c8627a
Enrico <franciscono.enry@gmail.com>
2021-02-22 15:43:19 +0100

    temporarly disabled bandcamp cause broken

--- src/commands/sources/template.bandcamp.js
+++ src/commands/sources/template.bandcamp.js
@@ -1,3 +1,6 @@
+// currently disabled cause broken
+
+
 // SimpleMusic - Source
 const config = require("../../config");
 
@@ -14,13 +17,16 @@ exports.getStream = async (url) => {
 }
 
 exports.getInfo = (url) => {
+    console.log(url);
     return new Promise(async (resolve, reject) => {
         let tracks = [];
         if (/(https?:\/\/)?\w+\.bandcamp.com\/album\/.+/.test(url)) { // If album add all songs
-            await bcfetch.getAlbumInfo(url).then(res => res.tracks.forEach(song => tracks.push({title: song.name, url: song.url, author:res.artist.name, authorUrl:res.artist.url, thumbnail:res.imageUrl, duration:parseInt(song.duration)})));
+            await bcfetch.getAlbumInfo(url).then(res => res.tracks.forEach(song => tracks.push({title: song.name, url: song.url, author:res.artist.name, authorUrl:res.artist.url, thumbnail:res.imageUrl, duration:song.duration})));
         } else { // If track
-            await bcfetch.getTrackInfo(url).then(res => tracks.push({title: res.name, url: res.url, author:res.artist.name, authorUrl:res.artist.url, thumbnail:res.imageUrl, duration:parseInt(res.duration)}))
+            await bcfetch.getTrackInfo(url).then(console.log);
+            await bcfetch.getTrackInfo(url).then(res => tracks.push({title: res.name, url: res.url, author:res.artist.name, authorUrl:res.artist.url, thumbnail:res.imageUrl, duration:res.duration}))
         }
+        console.log("tracks", tracks)
         resolve(tracks);
     });
 }
