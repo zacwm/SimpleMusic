a6b43c2b6c4b1108628539fd8456d301ad653580
Zachary <me@zachary.fun>
2021-08-29 20:26:08 +1000

    More ESLint! yay \o/

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,21 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    "standard"
+    "standard",
   ],
   parserOptions: {
-    ecmaVersion: 12
+    ecmaVersion: 12,
   },
+  ignorePatterns: ["**/old-commands/*.js"],
   rules: {
     indent: ["error", 2],
     semi: ["error", "always"],
     "eol-last": ["error", "never"],
     quotes: ["error", "double"],
-    "arrow-parens": ["error", "always"]
-  }
+    "arrow-parens": ["error", "always"],
+    "comma-dangle": ["error", "always-multiline"],
+  },
 };
